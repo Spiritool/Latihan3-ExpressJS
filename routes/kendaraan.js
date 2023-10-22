61db21fef36baebda02709d3b74948fca6c40c0f
Spiritool <mohammadshobri45@gmail.com>
2023-10-22 17:04:30 +0700

    dasxasws

--- routes/kendaraan.js
+++ routes/kendaraan.js
@@ -106,6 +106,63 @@ router.get('/(:id)', function (req, res) {
     })
 })
 
+router.patch('/update/:id', upload.single("gambar_kendaraan") ,[
+    body('no_pol').notEmpty(),
+    body('nama_kendaraan').notEmpty(),
+    body('id_transmisi').notEmpty(),
+], (req, res) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(422).json({
+            error: error.array()
+        });
+    }
+    let id = req.params.id;
+    let gambar_kendaraan = req.file ? req.file.filename : null;
+
+    connection.query(`select * from kendaraan where no_pol = ${id}`, function (err, rows) {
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server Error',
+            })
+        }
+        if(rows.lenght <=0){
+            return res.status(404).json({
+                status: false,
+                message: 'Not Found',
+            })
+        }
+        const namaFileLama = rows[0].gambar_kendaraan;
+
+        if(namaFileLama && gambar_kendaraan) {
+            const pathFileLama = path.join(__dirname, '../public/images', namaFileLama);
+            fs.unlinkSync(pathFileLama)
+        }
+  
+    let Data = {
+        no_pol: req.body.no_pol,
+        nama_kendaraan: req.body.nama_kendaraan,
+        id_transmisi: req.body.id_transmisi,
+        gambar_kendaraan: gambar_kendaraan
+    }   
+    connection.query(`update kendaraan set ? where no_pol = ${id}`, Data, function (err, rows) {
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server Error',
+                error:err
+            })
+        } else {
+            return res.status(200).json({
+                status: true,
+                message: 'Update Success..!',
+            })
+        }
+    })
+})  
+})
+
 
 
 router.delete('/delete/(:id)', function(req, res){
@@ -125,7 +182,7 @@ router.delete('/delete/(:id)', function(req, res){
                 message: 'Not Found',
             })
         }
-        const namaFileLama = rows[0].gambar;
+        const namaFileLama = rows[0].gambar_kendaraan;
 
         if(namaFileLama) {
             const pathFileLama = path.join(__dirname, '../public/images', namaFileLama);
